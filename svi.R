d4e3f537a41bae7f7f689ad44d61c7844fd40fdd
drickless <drickless1@gmail.com>
2017-11-20 18:29:51 -0600

    fix missing values, cardinality apply pca summarize results

--- svi.R
+++ svi.R
@@ -1,9 +1,33 @@
-install.packages("prcomp")
+install.packages("stats")
 
 library(tidyverse)
-library(prcomp)
+
 
 setwd("~/Documents/GitHub/ChathamData")
 census <- read_csv("censusClean.csv", na="")
 census <- as_data_frame(census)
 
+colnames(census)
+
+#replace missing values with median
+sapply(census,function(x) sum(is.na(x)))
+census$PerCapInc[is.na(census$PerCapInc)] <- median(census$PerCapInc,na.rm = T)
+census$MedValue[is.na(census$MedValue)] <- median(census$MedValue,na.rm = T)
+
+#reverse direction of vars so higher values for all = higher vuln
+census_trans <- transform(census,
+                          PCollegePlus=-PCollegePlus,
+                          MedValue = -MedValue,
+                          PerCapInc = -PerCapInc)
+#log transform
+#log.census <- log(census_trans[5:18])
+
+
+#apply pca
+censusPCA <- prcomp(census_trans[5:18],
+                    center = TRUE,
+                    scale. = TRUE)
+#look at results
+print(censusPCA)
+plot(censusPCA, type = "lines")
+summary(censusPCA)
