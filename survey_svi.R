21a8007e794d685b100946a22b9882d4a0782c83
drickless <drickless1@gmail.com>
2017-11-22 21:17:43 -0600

    fit linear models

--- survey_svi.R
+++ survey_svi.R
@@ -5,19 +5,16 @@ setwd("~/Documents/GitHub/ChathamData")
 surveySVI <- read_csv("surveySVI.csv", na = "")
 surveydf <- as_data_frame(surveySVI)
 
-surv <- select(surveydf, Evacuate,KnowEstab,SLRPers,SSDPers,ARelocate,Aprotect,ANoRecover,PCAbleMov,PCNoRecov,Age_Cat,Hincome,Education,Ethnicity,HomeLang,FldRisk,Gender,svi_outp_3)
+surv <- select(surveydf, Evacuate,KnowEstab,SLRPers,SSDPers,ARelocate,Aprotect,ANoRecover,PCAbleMov,PCNoRecov,Age_Cat,Hincome,Education,Ethnicity,HomeLang,FldRisk,Gender,SVI)
 
+fit1 <- lm(SVI ~ as.factor(PCNoRecov), data=surv)
+fit2 <- lm(SVI ~ as.factor(ANoRecover),data=surv)
+fit3 <- lm(SVI ~ as.factor(PCAbleMov),data=surv)
+fit4 <- lm(SVI ~ as.factor(Aprotect),data=surv)
+fit5 <- lm(SVI ~ as.factor(ARelocate),data=surv)
+fit6 <- lm(SVI ~ as.factor(Hincome),data=surv)
+fit7 <- lm(SVI ~ as.factor(Education),data=surv)
+fit8 <- lm(SVI ~ as.factor(Ethnicity),data=surv)
+fit9 <- lm(SVI ~ ANoRecover, data=surv)
 
-
-fit1 <- lm(svi_outp_3 ~ as.factor(PCNoRecov), data=surv)
-fit2 <- lm(svi_outp_3 ~ as.factor(ANoRecover),data=surv)
-fit3 <- lm(svi_outp_3 ~ as.factor(PCAbleMov),data=surv)
-fit4 <- lm(svi_outp_3 ~ as.factor(Aprotect),data=surv)
-fit5 <- lm(svi_outp_3 ~ as.factor(ARelocate),data=surv)
-fit6 <- lm(svi_outp_3 ~ Hincome,data=surv)
-fit7 <- lm(svi_outp_3 ~ Education,data=surv)
-fit8 <- lm(svi_outp_3 ~ as.factor(Ethnicity),data=surv)
-          
-summary(fit3)
-
-summary(fit2)
+summary(fit1)
