fddba67395534e82217697b689ab306e6614705f
drickless <drickless1@gmail.com>
2018-01-19 14:33:45 -0500

    update for 2016 ACS tract data redo PCA with full sovi dataset

--- svi.R
+++ svi.R
@@ -1,40 +1,42 @@
+#script for constructing a weighted social vulnerability index from acs data
+
 library(tidyverse)
 
 setwd("~/Documents/GitHub/ChathamData")
-census <- read_csv("censusClean.csv", na="")
-census <- as_data_frame(census)
+#census <- read_csv("acsdfClean.csv", na="")
+#cemsus <- as_data_frame(acsdf)
 
-colnames(census)
+acs <- read_csv("SVIdata2.csv", na="")
+acsdf <- as_data_frame(acs)
+
+colnames(acsdf)
 
 #replace missing values with median
-sapply(census,function(x) sum(is.na(x)))
-census$PerCapInc[is.na(census$PerCapInc)] <- median(census$PerCapInc,na.rm = T)
-census$MedValue[is.na(census$MedValue)] <- median(census$MedValue,na.rm = T)
+sapply(acsdf,function(x) sum(is.na(x)))
+acsdf$MedRent[is.na(acsdf$MedRent)] <- median(acsdf$MedRent,na.rm = T)
+acsdf$MedVal[is.na(acsdf$MedVal)] <- median(acsdf$MedVal,na.rm = T)
 
 #reverse direction of vars so higher values for all = higher vuln
-census_trans <- transform(census,
-                          PCollegePlus=-PCollegePlus,
-                          MedValue = -MedValue,
-                          PerCapInc = -PerCapInc)
-#log transform
-#log.census <- log(census_trans[5:18])
+acsdf_trans <- transform(acsdf,
+                          MedRent=-MedRent,
+                          MedVal = -MedVal,
+                          PercapInc = -PercapInc,
+                          PercHighInc = -PercHighInc)
 
-
-
-all <- census_trans[,5:18]
+all <- acsdf_trans[,7:32]
 #apply pca
-censusPCA <- prcomp(all,
+acsdfPCA <- prcomp(all,
                     center = TRUE,
                     scale. = TRUE)
 
 #look at results in different ways
-print(censusPCA)
-plot(censusPCA, type = "lines")
-summary(censusPCA)
-biplot(censusPCA, scale=0, cex=c(1,0.7))
+print(acsdfPCA)
+plot(acsdfPCA, type = "lines")
+summary(acsdfPCA)
+biplot(acsdfPCA, scale=0, cex=c(1,0.7))
 
 #compute st. dev. and variance
-stddev <- censusPCA$sdev
+stddev <- acsdfPCA$sdev
 pr_var <- stddev^2
 pr_var[1:10]
 
@@ -44,37 +46,38 @@ propvarex
 plot(propvarex, xlab = "Principal Comp", ylab = "Prop of Var Expl", type = "b")
 plot(cumsum(propvarex), xlab = "Principal Comp", ylab = "Cum Prop of Var Expl", type = "b")
 
-#Kaiser eigenvalue-greater-than-one rule
-plot(censusPCA, type="line")
+#Kaiser criterion
+plot(acsdfPCA, type="line")
 abline(h=1,col="red")
-censusPCA$sdev^2
-#4 components meet the criterion 
+acsdfPCA$sdev^2 #eigenvalues
+#7 components meet the criterion 
 
-PCArot <- censusPCA$rotation[,1:4]
-#PCArot_trans <- as.matrix(transform(PCArot,PC1=-PC1))
+
 
 #varimax rotation
-#vmx <- varimax(PCArot_trans,normalize=FALSE)
+PCArot <- acsdfPCA$rotation[,1:7] #subset selected components ("rotations" = loadings)
 vmx <- varimax(PCArot,normalize=FALSE)
 
+
 #scores
-scores<-top4<-censusPCA$x[,1:4]
-scoresTrans <- transform(as_data_frame(scores),PC1=-PC1) #change cardinality of PC1
+scores<-top7<-acsdfPCA$x[,1:7]
+scoresTrans <- transform(as_data_frame(scores),
+                         PC1=-PC2,
+                         PC3=-PC3,
+                         PC6=abs(PC6)) #change cardinality of components that decrease vuln
 
 #apply varimax rotation to scores
-#scores_rotate<-scores %*% vmx$rotmat
-scores_rotate <- as.matrix(scoresTrans) %*% vmx$rotmat
+scores_rotate <- as.matrix(scores) %*% vmx$rotmat
 
 #use proportion of variance explained as weighting scheme
-var_perc<-data.matrix(propvarex[1:4])
-#data.matrix(scores_rotate)
+var_perc<-data.matrix(propvarex[1:7])
 
 #calculate weighted sum and apply z standardization
 sumscores<-scale(data.matrix(scores_rotate)  %*% var_perc)
-Svi <- cbind(census_trans[,(1:4)],sumscores)
+Svi <- cbind(acsdf_trans[,(1:4)],sumscores) #append to identifying info
 
 #write to csv
-write.csv(Svi,file = "svi_output.csv",row.names = FALSE)
+write.csv(Svi,file = "svi_output2.csv",row.names = FALSE)
 
 
 
